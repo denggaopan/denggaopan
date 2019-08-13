2c497b35b873bc11e34d28b4f1009a1e0fc6dfe8
denggaopan <denggaopan@outlook.com>
2019-08-13 17:29:15 +0800

    简单的http请求工具

---
+++ Denggaopan.Net.Http/HttpHelper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Denggaopan.Net.Http
+{
+    public class HttpHelper
+    {
+        public static async Task<string> GetAsync(string url)
+        {
+            HttpClient client = new HttpClient();
+            var r = await client.GetAsync(url);
+            if (r.IsSuccessStatusCode)
+            {
+                var s = r.Content.ReadAsStringAsync().Result;
+                return s;
+            }
+            return null;
+        }
+
+        public static async Task<string> PostAsync(string url, string json)
+        {
+            HttpClient client = new HttpClient();
+            var content = new StringContent(json);
+            var r = await client.PostAsync(url, content);
+            if (r.IsSuccessStatusCode)
+            {
+                var s = r.Content.ReadAsStringAsync().Result;
+                return s;
+            }
+            return null;
+        }
+
+        public static async Task<string> PutAsync(string url, string json)
+        {
+            HttpClient client = new HttpClient();
+            var content = new StringContent(json);
+            var r = await client.PutAsync(url, content);
+            if (r.IsSuccessStatusCode)
+            {
+                var s = r.Content.ReadAsStringAsync().Result;
+                return s;
+            }
+            return null;
+        }
+
+        public static async Task<string> DeleteAsync(string url)
+        {
+            HttpClient client = new HttpClient();
+            var r = await client.DeleteAsync(url);
+            if (r.IsSuccessStatusCode)
+            {
+                var s = r.Content.ReadAsStringAsync().Result;
+                return s;
+            }
+            return null;
+        }
+    }
+}
---
+++ Denggaopan.Net.Http/IApiResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Denggaopan.Net.Http
+{
+    public interface IApiResult<T> where T : class
+    {
+        int Code { get; set; }
+        string Message { get; set; }
+        T Data { get; set; }
+    }
+    public class ApiResult : IApiResult<object>
+    {
+        public int Code { get; set; }
+        public string Message { get; set; }
+        public object Data { get; set; }
+    }
+
+    public class ApiResult<T> : IApiResult<T> where T : class
+    {
+        public int Code { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+    }
+}
