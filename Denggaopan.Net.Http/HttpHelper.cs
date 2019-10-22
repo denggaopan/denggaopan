a88af3b322aa0f8d89f05aca31ec1174566d0f7b
denggaopan <denggaopan@outlook.com>
2019-10-22 14:23:44 +0800

    http请求方法添加header参数

--- Denggaopan.Net.Http/HttpHelper.cs
+++ Denggaopan.Net.Http/HttpHelper.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace Denggaopan.Net.Http
@@ -8,52 +10,154 @@ namespace Denggaopan.Net.Http
     {
         public static async Task<string> GetAsync(string url)
         {
-            HttpClient client = new HttpClient();
-            var r = await client.GetAsync(url);
-            if (r.IsSuccessStatusCode)
+            using (HttpClient client = new HttpClient())
             {
-                var s = r.Content.ReadAsStringAsync().Result;
-                return s;
+                var r = await client.GetAsync(url);
+                if (r.IsSuccessStatusCode)
+                {
+                    var s = r.Content.ReadAsStringAsync().Result;
+                    return s;
+                }
+                return null;
             }
-            return null;
+        }
+
+        public static async Task<string> GetAsync(string url, Dictionary<string, string> headers)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+
+                if (headers.Count > 0)
+                {
+                    foreach (var header in headers)
+                    {
+                        client.DefaultRequestHeaders.Add(header.Key, header.Value);
+                    }
+                }
+
+                var r = await client.GetAsync(url);
+                if (r.IsSuccessStatusCode)
+                {
+                    var s = r.Content.ReadAsStringAsync().Result;
+                    return s;
+                }
+                return null;
+            }
         }
 
         public static async Task<string> PostAsync(string url, string json)
         {
-            HttpClient client = new HttpClient();
-            var content = new StringContent(json);
-            var r = await client.PostAsync(url, content);
-            if (r.IsSuccessStatusCode)
+            using (HttpClient client = new HttpClient())
             {
-                var s = r.Content.ReadAsStringAsync().Result;
-                return s;
+                var content = new StringContent(json);
+                var r = await client.PostAsync(url, content);
+                if (r.IsSuccessStatusCode)
+                {
+                    var s = r.Content.ReadAsStringAsync().Result;
+                    return s;
+                }
+                return null;
             }
-            return null;
+        }
+
+        public static async Task<string> PostAsync(string url, Dictionary<string, string> headers, string json)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+
+                if (headers.Count > 0)
+                {
+                    foreach (var header in headers)
+                    {
+                        client.DefaultRequestHeaders.Add(header.Key, header.Value);
+                    }
+                }
+
+                var content = new StringContent(json);
+                var r = await client.PostAsync(url, content);
+                if (r.IsSuccessStatusCode)
+                {
+                    var s = r.Content.ReadAsStringAsync().Result;
+                    return s;
+                }
+                return null;
+            }
         }
 
         public static async Task<string> PutAsync(string url, string json)
         {
-            HttpClient client = new HttpClient();
-            var content = new StringContent(json);
-            var r = await client.PutAsync(url, content);
-            if (r.IsSuccessStatusCode)
+            using (HttpClient client = new HttpClient())
             {
-                var s = r.Content.ReadAsStringAsync().Result;
-                return s;
+                var content = new StringContent(json);
+                var r = await client.PutAsync(url, content);
+                if (r.IsSuccessStatusCode)
+                {
+                    var s = r.Content.ReadAsStringAsync().Result;
+                    return s;
+                }
+                return null;
             }
-            return null;
+        }
+
+        public static async Task<string> PutAsync(string url, Dictionary<string, string> headers, string json)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+
+                if (headers.Count > 0)
+                {
+                    foreach (var header in headers)
+                    {
+                        client.DefaultRequestHeaders.Add(header.Key, header.Value);
+                    }
+                }
+
+                var content = new StringContent(json);
+                var r = await client.PutAsync(url, content);
+                if (r.IsSuccessStatusCode)
+                {
+                    var s = r.Content.ReadAsStringAsync().Result;
+                    return s;
+                }
+                return null;
+            }
         }
 
         public static async Task<string> DeleteAsync(string url)
         {
-            HttpClient client = new HttpClient();
-            var r = await client.DeleteAsync(url);
-            if (r.IsSuccessStatusCode)
+            using (HttpClient client = new HttpClient())
             {
-                var s = r.Content.ReadAsStringAsync().Result;
-                return s;
+                var r = await client.DeleteAsync(url);
+                if (r.IsSuccessStatusCode)
+                {
+                    var s = r.Content.ReadAsStringAsync().Result;
+                    return s;
+                }
+                return null;
             }
-            return null;
+        }
+
+        public static async Task<string> DeleteAsync(string url, Dictionary<string, string> headers)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+
+                if (headers.Count > 0)
+                {
+                    foreach (var header in headers)
+                    {
+                        client.DefaultRequestHeaders.Add(header.Key, header.Value);
+                    }
+                }
+
+                var r = await client.DeleteAsync(url);
+                if (r.IsSuccessStatusCode)
+                {
+                    var s = r.Content.ReadAsStringAsync().Result;
+                    return s;
+                }
+                return null;
+            }
         }
     }
 }
