30f75ff0070903ee09008ae7aa6313f8453530b0
denggaopan <denggaopan@outlook.com>
2019-11-27 18:13:36 +0800

    APIGateway demo

---
+++ APIGatewayConsulDemo/OrderService/Controllers/HealthController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrderService.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "OK";
+        }
+    }
+}
---
+++ APIGatewayConsulDemo/OrderService/Controllers/OrderController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrderService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        // GET: api/Order
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Order1", "Order2" };
+        }
+
+        // GET: api/Order/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "order-"+id;
+        }
+
+        // POST: api/Order
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Order/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
